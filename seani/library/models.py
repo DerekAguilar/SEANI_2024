7d877b7a47d5436956daa62d6de8d196f37abe93
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2024-02-20 14:03:02 -0600

    02 App Library

---
+++ seani/library/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+# Create your models here.
+class Module(models.Model):
+    name=models.CharField(
+        max_length=100,
+        verbose_name="Nombre"
+    )
+    description=models.CharField(
+        max_length=200,
+        verbose_name="Descripción"
+    )
+    def num_questions(self):
+        return self.question_set.count()
+    
+    num_questions.short_description="Número de Preguntas"
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name="módulo"
+        verbose_name_plural="módulos"
+
+class Question(models.Model):
+    module=models.ForeignKey(
+        Module,
+        on_delete=models.CASCADE,
+        verbose_name="Módulo"
+    )
+    question_text=models.TextField(
+        verbose_name="Texto de Pregunta",
+        null=True,
+        blank=True
+    )
+    question_image=models.ImageField(
+        verbose_name="Imagen de Pregunta",
+        upload_to="questions",
+        null=True,
+        blank=True,
+    )
+    answer1=models.CharField(
+        max_length=200,
+        verbose_name="Respuesta A",
+    )
+    answer2=models.CharField(
+        max_length=200,
+        verbose_name="Respuesta B"
+    )
+    answer3=models.CharField(
+        max_length=200,
+        verbose_name="Respuesta C",
+        null=True,
+        blank=True
+    )
+    answer4=models.CharField(
+        max_length=200,
+        verbose_name="Respuesta D",
+        null=True,
+        blank=True
+    )
+    correct=models.CharField(
+        max_length=5,
+        verbose_name="Respuesta Correcta"
+    )
+    def __str__(self):
+        return f"{ self.module } - { self.id }"
+    class Meta:
+        verbose_name="pregunta"
+        verbose_name_plural="preguntas"
